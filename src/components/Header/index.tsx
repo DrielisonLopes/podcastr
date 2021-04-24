6143c67270d09bb39bd93de879584496060294f3
Drielison Lopes <drielisonl@gmail.com>
2021-04-24 14:18:11 -0300

    :tada: initial, final commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+import styles from './styles.module.scss'
+import format from 'date-fns/format'
+import ptBR from 'date-fns/locale/pt-BR'
+import Link from 'next/link'
+
+export function Header() {
+  const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
+    locale: ptBR
+  })
+
+  return (
+    <header className={styles.headerContainer}>
+      <Link href="/">
+          <button>
+            <img src="/logo.svg" alt="Podcastr"/>
+          </button>
+      </Link>
+      
+      <p>O melhor para você ouvir, sempre</p>
+
+      <span>{currentDate}</span>
+    </header>
+  )
+}
